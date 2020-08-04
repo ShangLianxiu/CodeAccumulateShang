72f3e581232f3ab40985a434de57e8146d7fe58d
shang <shang@DESKTOP-K5T2BAT>
2020-08-04 11:15:39 +0800

    添加人民币大写转换

---
+++ 金额相关/金额大写转换.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CodeAccumulateShang.金额相关
+{
+   public class 金额大写转换
+    {
+        /// <summary>
+        /// 人民币大写转换
+        /// </summary>
+        /// <param name="number">数字</param>
+        /// <returns>返回人民币大写,如：壹元整</returns>
+        public static string ConvertToChinese(decimal number)
+        {
+            var s = number.ToString("#L#E#D#C#K#E#D#C#J#E#D#C#I#E#D#C#H#E#D#C#G#E#D#C#F#E#D#C#.0B0A");
+            var d = Regex.Replace(s, @"((?<=-|^)[^1-9]*)|((?'z'0)[0A-E]*((?=[1-9])|(?'-z'(?=[F-L\.]|$))))|((?'b'[F-L])(?'z'0)[0A-L]*((?=[1-9])|(?'-z'(?=[\.]|$))))", "${b}${z}");
+            var r = Regex.Replace(d, ".", m => "负元空零壹贰叁肆伍陆柒捌玖空空空空空空空分角拾佰仟万亿兆京垓秭穰"[m.Value[0] - '-'].ToString());
+            return r;
+        }
+    }
+}
